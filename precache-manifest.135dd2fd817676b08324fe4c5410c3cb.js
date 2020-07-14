61af3dbce7589123c658f9f983afe696bfc8cb23
Michael Jacholke <mpj2@hw.ac.uk>
2020-07-14 11:18:23 +0100

    Updates

---
+++ precache-manifest.135dd2fd817676b08324fe4c5410c3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727cf672a8ecc44d1ff63daf449fa915",
+    "url": "/weather-dashboard/index.html"
+  },
+  {
+    "revision": "96f6a25259df6e2aff34",
+    "url": "/weather-dashboard/static/css/main.e2a5f0a8.chunk.css"
+  },
+  {
+    "revision": "99e9a6fa65a291d8cffd",
+    "url": "/weather-dashboard/static/js/2.161f1a9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-dashboard/static/js/2.161f1a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f6a25259df6e2aff34",
+    "url": "/weather-dashboard/static/js/main.78ebe988.chunk.js"
+  },
+  {
+    "revision": "2b5f95056f37d74217a3",
+    "url": "/weather-dashboard/static/js/runtime-main.44b4e27e.js"
+  }
+]);
