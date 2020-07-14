f0276df241bac041dc2b6873cc837d61cca3f018
Michael Jacholke <mpj2@hw.ac.uk>
2020-07-14 11:27:06 +0100

    link fix

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -19,7 +19,7 @@ export default class WeatherInfo extends Component {
     }
 
     getApiInformation = async (location) => {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`);
         const json = await response.json();
         console.log(json);
         this.setState({
